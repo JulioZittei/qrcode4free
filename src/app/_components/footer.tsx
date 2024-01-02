e835a197f8fbfbf3d2d2a18d946216add4a12de1
Julio Zittei <julio.zittei96@gmail.com>
2024-01-01 21:06:18 -0300

    feat: add footer component

---
+++ src/app/_components/footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="bg-slate-50 px-8 py-4 text-center text-sm">
+      © 2024 Copyright <span className="font-semibold">QRCode4Free</span>
+    </footer>
+  );
+}
+
+export default Footer;
